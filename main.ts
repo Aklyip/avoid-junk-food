e4823d2b04ccd2a0d1778a248233f2b80887ab95
Aklyip <81716556+Aklyip@users.noreply.github.com>
2021-04-07 17:39:48 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+let fishlefyImage=img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . . . 
+    . . . c 4 d 4 4 4 4 4 1 c . c c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+    . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+    . f 4 4 4 4 1 c 4 f 4 d f f f f 
+    . . f f 4 d 4 4 f f 4 c f c . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `
+let fishRightImage=fishlefyImage.clone()
+fishRightImage.flipX()
+let fish = sprites.create(fishlefyImage, SpriteKind.Player)
+fish.setPosition(80, 110)
+controller.moveSprite(fish, 100, 0)
+fish.setStayInScreen(true)
+controller.left.onEvent(ControllerButtonEvent.Pressed, function() {
+    fish.setImage(fishlefyImage)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function() {
+    fish.setImage(fishRightImage)
+})
